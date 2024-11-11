5ddd3adfc459b478f366e7f93c243ff3dd953047
Alexandre Silva <asilva1604@outlook.com>
2024-11-11 12:18:16 +0000

    add report model

---
+++ app/Models/Report.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Report extends Model
+{
+    use HasFactory;
+
+    protected $table = 'report';
+
+    protected $fillable = [
+        'reason',
+        'status',
+        'auction_id',
+        'user_id',
+    ];
+
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    // Define relationships
+
+    // A report belongs to an auction
+    public function auction() : BelongsTo
+    {
+        return $this->belongsTo(Auction::class, 'auction_id');
+    }
+
+    // A report is made by a user
+    public function user() : BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
