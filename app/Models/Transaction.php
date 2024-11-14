8a5d68e6fe4312f61dabb5584e8aca638d5ece86
Alexandre Silva <asilva1604@outlook.com>
2024-11-14 12:03:09 +0000

    add transaction model

---
+++ app/Models/Transaction.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Transaction extends Model
+{
+    use HasFactory;
+
+    protected $table = 'transaction';
+
+    protected $fillable = [
+        'amount',
+        'account_id',
+    ];
+
+    protected $casts = [
+        'amount' => 'int',
+        'account_id' => 'int',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    public function account() : BelongsTo {
+        return $this->belongsTo(User::class, 'account_id');
+    }
+}
