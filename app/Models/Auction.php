123f2a6a1c2644610bbf2b834524d7a28b3526ce
Alexandre Silva <asilva1604@outlook.com>
2024-11-14 12:28:59 +0000

    add some relations

--- app/Models/Auction.php
+++ app/Models/Auction.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
 
 class Auction extends Model
 {
@@ -18,6 +21,7 @@ class Auction extends Model
         'start_date',
         'end_date',
         'minimum_bid',
+        'current_bid',
         'category_id',
         'creator_id',
         'buyer_id',
@@ -25,8 +29,12 @@ class Auction extends Model
     ];
 
     protected $casts = [
+        'title' => 'string',
+        'description' => 'string',
         'start_date' => 'datetime',
         'end_date' => 'datetime',
+        'minimum_bid' => 'integer',
+        'current_bid' => 'integer',
         'created_at' => 'datetime',
         'updaded_at' => 'datetime',
     ];
@@ -50,6 +58,31 @@ class Auction extends Model
     public function buyer() : BelongsTo
     {
         return $this->belongsTo(User::class, 'buyer_id');
+    }
+
+    public function comments() : HasMany {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function reports() : HasMany {
+        return $this->hasMany(Report::class);
+    }
+
+    public function bids() : HasMany {
+        return $this->hasMany(Bid::class);
+    }
+
+    public function highestBid(): HasOne
+    {
+        return $this->hasOne(Bid::class)->orderBy('amount', 'desc');
+    }
+
+    public function rating() : HasOne {
+        return $this->hasOne(Rating::class);
+    }
+
+    public function transaction() : HasOne {
+        return $this->hasOne(Transaction::class);
     }
 
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -49,4 +49,8 @@ class User extends Authenticatable
         'password' => 'hashed',
         'birth_date' => 'date',
     ];
+
+    public function comments() : HasMany {
+        return $this->hasMany(Comment::class);
+    }
 }
