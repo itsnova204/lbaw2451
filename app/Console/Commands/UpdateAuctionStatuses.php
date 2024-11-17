dcf19738ad59a2e561ebe28526fe7b363040f25f
Alexandre Silva <asilva1604@outlook.com>
2024-11-17 12:24:45 +0000

    small corrections

--- app/Console/Commands/UpdateAuctionStatuses.php
+++ app/Console/Commands/UpdateAuctionStatuses.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands;
 
 use App\Models\Auction;
+use App\Models\Bid;
+use App\Models\Transaction;
 use Carbon\Carbon;
 use Illuminate\Console\Command;
 
@@ -31,8 +33,26 @@ class UpdateAuctionStatuses extends Command
 
         $affected = Auction::where('end_date', '<=', $now)
             ->where('status', 'active') // Assuming ACTIVE is the status of ongoing auctions
-            ->update(['status' => 'ended']); // Assuming COMPLETED represents ended auctions
+            ->get();// Assuming COMPLETED represents ended auctions
+
+        foreach ($affected as $auction) {
+            // Set the status to 'ended' once the auction is past its end date
+            $bid = Bid::where('auction_id', $auction->id)->where('amount', $auction->current_bid)->first();
+
+            $auction->status = 'ended';
+            $auction->buyer_id = $bid->user_id;
+            $auction->save();
+
+            $transaction = Transaction::create([
+                'amount' => $auction->current_bid,
+                'auction_id' => $auction->id,
+            ]);
+
+            $this->info("Auction '{$auction->title}' has been updated to 'ended', and the respective transaction has been created.");
+        }
 
         $this->info("$affected auctions have been updated to ENDED.");
+
+
     }
 }
