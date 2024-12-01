15581d0128ef271bb63d4d136460c51175502071
Alexandre Silva <asilva1604@outlook.com>
2024-12-01 13:20:41 +0000

    work in progress

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,5 +1,15 @@
-<form method="POST" action="{{ route('password.email') }}">
-    @csrf
-    <input type="email" name="email" required>
-    <button type="submit">Send Password Reset Link</button>
-</form>
+@extends('layouts.app')
+
+@section('title', 'Reset Password')
+
+@section('content')
+
+    <div class="rectangle-div">
+    <form method="POST" action="{{ route('password.email') }}">
+        @csrf
+        <label for="email" class="">Email</label>
+        <input type="email" name="email" required>
+        <button type="submit">Send Password Reset Link</button>
+    </form>
+    </div>
+@endsection
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,8 +1,20 @@
-<form method="POST" action="{{ route('password.update') }}">
-    @csrf
-    <input type="hidden" name="token" value="{{ $token }}">
-    <input type="email" name="email" required>
-    <input type="password" name="password" required>
-    <input type="password" name="password_confirmation" required>
-    <button type="submit">Reset Password</button>
-</form>
+@extends('layouts.app')
+
+@section('title', 'Reset Password')
+
+@section('content')
+    <div class="rectangle-div reset">
+        <form method="POST" action="{{ route('password.update') }}">
+            @csrf
+            <label for="email" class="">Email</label>
+            <input type="hidden" name="token" value="{{ $token }}">
+            <input type="email" name="email" required>
+            <label for="password" class="">Password</label>
+            <input type="password" name="password" required>
+            <label for="password_confirmation" class="">Confirm Password</label>
+            <input type="password" name="password_confirmation" required>
+            <button type="submit">Reset Password</button>
+        </form>
+    </div>
+
+@endsection
