dfc54b0f71a6d8ed8792243e76d20a76426ae7e3
Alexandre Silva <asilva1604@outlook.com>
2024-12-05 11:39:10 +0000

    more stylish

--- resources/views/pages/user/balance.blade.php
+++ resources/views/pages/user/balance.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Login')
 
 @section('content')
-    <div class="rectangle-div">
+    <div class="rectangle-div" id="my-balance">
         <h1>My Balance</h1>
         <h2>You have a balance of {{$user->balance}}€</h2>
         <a href=""><button>Deposit</button></a>
@@ -15,6 +15,11 @@
             @php($auction = $transaction->auction)
             <div class="rectangle-div transaction red">
                 <span>{{$auction->title}}</span>
+                <span>Seller:
+                    <a href="{{route('user.show', $auction->creator()->first())}}">
+                    {{$auction->creator()->first()->username}}
+                    </a>
+                </span>
                 <span>{{$transaction->created_at}}</span>
                 <span>{{$transaction->amount}}€</span>
             </div>
@@ -25,6 +30,11 @@
             @php($auction = $transaction->auction)
             <div class="rectangle-div transaction green">
                 <span>{{$auction->title}}</span>
+                <span>Buyer:
+                    <a href="{{route('user.show', $transaction->buyer()->first())}}">
+                    {{$transaction->buyer()->first()->username}}
+                    </a>
+                </span>
                 <span>{{$transaction->created_at}}</span>
                 <span>{{$transaction->amount}}€</span>
             </div>
